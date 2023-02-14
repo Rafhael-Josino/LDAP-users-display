3e313590ff1d9959107a2f87f291e247a515ff52
rafhael josino <rafhael_josino1@hotmail.com>
2023-02-13 21:25:26 -0300

    update with the use of Routes, the CSS and the backend API path

--- ldap_frontend/src/actions/index.tsx
+++ ldap_frontend/src/actions/index.tsx
@@ -1,8 +1,9 @@
+import axios from "axios";
 import ldapClient from "../APIs/ldap-client";
 
-async function requestVPNusers(barrackCode: string) {
+async function requestVPNusers(barrackCode: string, logFileName: string) {
     try {
-        const res = await ldapClient.get(`vpnUsers/${barrackCode}`);
+        const res = await ldapClient.get(`vpnUsers/${barrackCode}/${logFileName}`);
 
         console.log(res)
 
@@ -30,4 +31,27 @@ async function saveVPNusersCSV(vpnUsers: vpnUserType[]) {
     }
 }
 
-export { requestVPNusers, saveVPNusersCSV };
+async function saveLogCSV(formData: FormData) {
+    try {
+        const res = await axios.post("http://localhost:8000/vpnLog", formData, {});
+
+        return res;
+    } catch(err: any) { 
+        console.log('error:\n', err);
+        //return err.response.data.message;
+        return err;
+    }
+}
+
+async function getSavedLog() {
+    try {
+        return await ldapClient.get('vpnLog');
+    } catch(err: any) {
+        console.log('error:\n', err);
+        //return err.response.data.message;
+        return err;
+    }
+    
+}
+
+export { requestVPNusers, saveVPNusersCSV, saveLogCSV, getSavedLog };
--- ldap_frontend/src/index.tsx
+++ ldap_frontend/src/index.tsx
@@ -1,6 +1,15 @@
 import "./index.css";
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './components/App';
 
+const router = createBrowserRouter([
+    {
+        path: "/*", 
+        element: <App />,
+    },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
-root.render(<App />);
+
+root.render(<RouterProvider router={router} />);
